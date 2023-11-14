5abf6bd2b3714c6bc850700a7210295d342d49a9
Sergey Kiselev <vvh1te@mail.ru>
2023-11-14 11:13:43 +0000

    add variants film models

--- fastapi-solution/src/models/film.py
+++ fastapi-solution/src/models/film.py
@@ -1,11 +1,18 @@
 from models.templ_model import IdMix
 from models.other_model import Genre,Person
+from pydantic.typing import Literal
 
-class Film(IdMix):
-    imdb_rating: str | float | None
+
+class FilmBase(IdMix):
     title: str
+    imdb_rating: str | float | None
+
+class Film(FilmBase):  
     description: str | None
     genres: list[Genre]
     directors: list[Person]
     actors: list[Person]
     writers: list[Person]
+
+class FilmPerson(IdMix):
+    roles  : Literal['actor','writer','director']
