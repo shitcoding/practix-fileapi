84433ed6b81b42004c17141f988249350f9c7fa7
Амир Ахметжанов <amirah@halykbank.kz>
2023-11-14 18:27:46 +0600

    Добавил сервис кэширования v2

---
+++ fastapi-solution/src/dependencies.py
@@ -0,0 +1,23 @@
+from redis.asyncio import Redis
+from elasticsearch import AsyncElasticsearch
+
+from core import config
+
+redis = Redis(host=config.REDIS_HOST, port=config.REDIS_PORT)
+es = AsyncElasticsearch(hosts=[f'{config.ELASTIC_SCHEME}://{config.ELASTIC_HOST}:{config.ELASTIC_PORT}'])
+
+
+async def get_redis() -> Redis:
+    client = Redis(host=config.REDIS_HOST, port=config.REDIS_PORT)
+    try:
+        yield client
+    finally:
+        await client.close()
+
+
+async def get_elasticsearch() -> AsyncElasticsearch:
+    client = AsyncElasticsearch(hosts=[f'{config.ELASTIC_SCHEME}://{config.ELASTIC_HOST}:{config.ELASTIC_PORT}'])
+    try:
+        yield client
+    finally:
+        await client.close()
--- fastapi-solution/src/main.py
+++ fastapi-solution/src/main.py
@@ -1,15 +1,12 @@
-import logging
+from contextlib import asynccontextmanager
 
 import uvicorn
-from elasticsearch import AsyncElasticsearch
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
-from redis.asyncio import Redis
 
-from api.v1 import films
+from dependencies import get_redis, get_elastic
+from api.v1.films import router
 from core import config
-from core.logger import LOGGING
-from db import elastic_storage, redis_cache
 
 app = FastAPI(
     title=config.PROJECT_NAME,
@@ -18,22 +15,12 @@ app = FastAPI(
     default_response_class=ORJSONResponse,
 )
 
-
-@app.on_event('startup')
-async def startup():
-    redis_cache.redis = Redis(host=config.REDIS_HOST, port=config.REDIS_PORT)
-    elastic_storage.es = AsyncElasticsearch(hosts=[f'{config.ELASTIC_SCHEME}://{config.ELASTIC_HOST}:{config.ELASTIC_PORT}'])
-
-
-@app.on_event('shutdown')
-async def shutdown():
-    await redis_cache.redis.close()
-    await elastic_storage.es.close()
-
-
 # Подключаем роутер к серверу, указав префикс /v1/films
 # Теги указываем для удобства навигации по документации
-app.include_router(films.router, prefix='/api/v1/films', tags=['films'])
+app.include_router(router, prefix='/api/v1/films', tags=['films'])
+
+
+
 
 if __name__ == '__main__':
     uvicorn.run(
