d8e9f2aec6c52bbddce09846c8ded6d95e743fed
Vera Kaynara <vera.blinova.1996@gmail.com>
2023-11-17 22:41:34 +0700

    Add exceptions

--- fastapi-solution/src/api/v1/genres.py
+++ fastapi-solution/src/api/v1/genres.py
@@ -1,3 +1,4 @@
+import logging
 from http import HTTPStatus
 
 from core.config import DEFAULT_PAGE_SIZE
@@ -7,17 +8,24 @@ from services.genre import GenreService, get_genre_service
 
 router = APIRouter()
 
+logger = logging.getLogger(__name__)
 
 @router.get('/{uuid}', response_model=Genre)
 async def genre_name(uuid: str, genre_service: GenreService = Depends(get_genre_service)) -> Genre:
-    genre = await genre_service.get_by_id(uuid)
-    if not genre:
-        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='genre not found')
+    try:
+        genre = await genre_service.get_by_id(uuid)
+    except Exception as e:
+        logger.error(e)
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='Genre not found')
     return genre
 
 
 @router.get('/', response_model=list[Genre])
 async def genres_list(query: str = "", sort: str = "", page: int = 1, size: int = DEFAULT_PAGE_SIZE,
                       genre_service: GenreService = Depends(get_genre_service)) -> list[Genre]:
-    genres = await genre_service.search(query=query, sort=sort, page=page, size=size)
+    try:
+        genres = await genre_service.search(query=query, sort=sort, page=page, size=size)
+    except Exception as e:
+        logger.error(e)
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='Genres not found')
     return genres
--- fastapi-solution/src/api/v1/persons.py
+++ fastapi-solution/src/api/v1/persons.py
@@ -1,3 +1,4 @@
+import logging
 from http import HTTPStatus
 
 from core.config import DEFAULT_PAGE_SIZE
@@ -8,24 +9,35 @@ from services.person import PersonService, get_person_service
 
 router = APIRouter()
 
+logger = logging.getLogger(__name__)
 
 @router.get('/{uuid}', response_model=PersonFilms)
 async def person_name(uuid: str, person_service: PersonService = Depends(get_person_service)) -> PersonFilms:
-    person = await person_service.get_by_id(uuid)
-    if not person:
-        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='person not found')
+    try:
+        person = await person_service.get_by_id(uuid)
+    except Exception as e:
+        logger.error(e)
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='Person not found')
     return person
 
 
 @router.get('/search/', response_model=list[PersonFilms])
 async def persons_list(query: str = "", sort: str = "", page: int = 1, size: int = DEFAULT_PAGE_SIZE,
                        person_service: PersonService = Depends(get_person_service)) -> list[PersonFilms]:
-    persons = await person_service.search(query=query, sort=sort, page=page, size=size)
+    try:
+        persons = await person_service.search(query=query, sort=sort, page=page, size=size)
+    except Exception as e:
+        logger.error(e)
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='Persons not found')
     return persons
 
 
 @router.get('/{uuid}/films', response_model=list[FilmBase])
 async def person_films(uuid: str, sort: str = "", page: int = 1, size: int = DEFAULT_PAGE_SIZE,
                        person_service: PersonService = Depends(get_person_service)) -> list[FilmBase]:
-    films = await person_service.film_search(uuid=uuid, sort=sort, page=page, size=size)
+    try:
+        films = await person_service.film_search(uuid=uuid, sort=sort, page=page, size=size)
+    except Exception as e:
+        logger.error(e)
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='Films not found')
     return films
