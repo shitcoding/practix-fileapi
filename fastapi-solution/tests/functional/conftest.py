5362b8965b1ee675198ca23984ce583aed4c3405
Vera Kaynara <vera.blinova.1996@gmail.com>
2023-12-03 17:12:09 +0700

    Add tests for genres

--- fastapi-solution/tests/functional/conftest.py
+++ fastapi-solution/tests/functional/conftest.py
@@ -5,10 +5,11 @@ import aiohttp
 import pytest_asyncio
 from elasticsearch import AsyncElasticsearch
 from elasticsearch.helpers import async_bulk
-from tests.functional.settings import get_settings
+from functional.settings import get_settings
 
 from functional.logger import logger
-from functional.testdata.movies_data import TEST_MOVIE_DATA
+from functional.testdata.es_mapping import SETTINGS
+from functional.testdata.movies_data import TEST_MOVIE_DATA, TEST_GENRE_DATA
 
 settings = get_settings()
 DataGenerator = Callable[[], list[dict[str, any]]]
@@ -23,6 +24,7 @@ def event_loop():
 
 @pytest_asyncio.fixture(name='es_client', scope='session')
 async def es_client():
+    logger.info('Connecting to elasticsearch...')
     es_client = AsyncElasticsearch(
         hosts=f'http://{settings.elastic.es_host}:{settings.elastic.es_port}',
         verify_certs=False,
@@ -33,8 +35,10 @@ async def es_client():
 
 @pytest_asyncio.fixture(name='asyncio_client', scope='session')
 async def asyncio_client():
+    logger.info('Connecting asyncio client...')
     session = aiohttp.ClientSession()
     yield session
+    logger.info('Closing asyncio client...')
     await session.close()
 
 
@@ -48,6 +52,9 @@ async def es_data_loader(
         data_generator: DataGenerator,
         mapping: dict,
     ):
+        mapping = {'settings': SETTINGS, 'mappings': mapping}
+
+        logger.info(f'Filling index {index_name}...')
         es_data = data_generator()
         bulk_query = []
         for row in es_data:
@@ -66,7 +73,16 @@ async def es_data_loader(
 @pytest_asyncio.fixture(scope='session')
 def movie_data_generator():
     def generate_data():
+        logger.info('Generating movie data...')
         return [TEST_MOVIE_DATA for _ in range(60)]
+    return generate_data
+
+
+@pytest_asyncio.fixture(scope='session')
+def genre_data_generator():
+    def generate_data():
+        logger.info('Generating genre data...')
+        return TEST_GENRE_DATA
     return generate_data
 
 
@@ -74,7 +90,9 @@ def movie_data_generator():
 def es_write_data(es_client: AsyncElasticsearch):
     async def inner(data: list[dict], index_mapping: dict, elastic_index: str):
         if await es_client.indices.exists(index=elastic_index):
+            logger.info(f'Index {elastic_index} already exists. Deleting index {elastic_index}...')
             await es_client.indices.delete(index=elastic_index)
+        logger.info(f'Creating index {elastic_index}...')
         await es_client.indices.create(
             index=elastic_index,
             **index_mapping,
@@ -91,9 +109,11 @@ def es_write_data(es_client: AsyncElasticsearch):
 @pytest_asyncio.fixture(name='make_get_request')
 async def make_get_request(asyncio_client: aiohttp.ClientSession):
     async def inner(path, query_data):
+        logger.info(f'GET {path}?{query_data}')
         url = f'http://{settings.fastapi.app_host}:{settings.fastapi.app_port}/api/v1/{path}'
 
         async with asyncio_client.get(url, params=query_data) as response:
+            logger.info(f'response: {await response.json()}')
             return {
                 'body': await response.json(),
                 'headers': response.headers,
---
+++ fastapi-solution/tests/functional/src/test_genre.py
@@ -0,0 +1,28 @@
+import pytest
+
+from functional.logger import logger
+from functional.settings import get_settings
+from functional.testdata.es_mapping import GENRE_MAPPING
+
+
+test_settings = get_settings()
+
+
+@pytest.mark.asyncio
+async def test_genre(es_data_loader, make_get_request, genre_data_generator):
+    await es_data_loader(
+        index_name=test_settings.elastic.es_genres_index,
+        data_generator=genre_data_generator,
+        mapping=GENRE_MAPPING,
+    )
+    query_data = {'query': 'Comedy'}
+    response = await make_get_request(
+        path='genres/',
+        query_data=query_data,
+    )
+
+    status = response.get('status')
+    body = response.get('body')
+    logger.info(f'answer: {body}')
+    assert status == 200
+    assert len(body) == 50
