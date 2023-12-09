00ef59bad99489a70f932914a5c3beb0418a5ebe
Sergey Kiselev <vvh1te@mail.ru>
2023-12-09 12:21:31 +0000

    task done

--- fastapi-solution/tests/functional/utils/wait_for_es.py
+++ fastapi-solution/tests/functional/utils/wait_for_es.py
@@ -1,5 +1,5 @@
 import time
-
+import backoff 
 from elasticsearch import ConnectionError, Elasticsearch, NotFoundError
 
 from functional.logger import logger
@@ -7,16 +7,16 @@ from functional.settings import get_settings
 
 settings = get_settings()
 
+@backoff.on_exception(backoff.constant,(ConnectionError, NotFoundError),max_tries=5)
+def es_ping(client):
+    logger.info("Waiting for Elasticsearch...")
+    client.ping()
+    
+
 if __name__ == '__main__':
     es_client = Elasticsearch(
         f'http://{settings.elastic.es_host}:{settings.elastic.es_port}'
     )
+    es_ping(es_client)
+    logger.info("Elasticsearch is up and running!")
 
-    while True:
-        try:
-            if es_client.ping():
-                logger.info("Elasticsearch is up and running!")
-                break
-        except (ConnectionError, NotFoundError):
-            logger.info("Waiting for Elasticsearch...")
-            time.sleep(1)
--- fastapi-solution/tests/functional/utils/wait_for_redis.py
+++ fastapi-solution/tests/functional/utils/wait_for_redis.py
@@ -1,5 +1,5 @@
 import time
-
+import backoff 
 import redis
 
 from functional.logger import logger
@@ -7,18 +7,15 @@ from functional.settings import get_settings
 
 settings = get_settings()
 
+@backoff.on_exception(backoff.constant,(redis.exceptions.ConnectionError,redis.exceptions.BusyLoadingError),max_tries=5)
+def redis_ping(client):
+    logger.info("Waiting for Redis...")
+    client.ping()
+
 if __name__ == '__main__':
     redis_host = settings.redis.redis_host
     redis_port = settings.redis.redis_port
-    while True:
-        try:
-            r = redis.Redis(host=redis_host, port=redis_port)
-            if r.ping():
-                logger.info("Redis is up and running!")
-                break
-        except (
-                redis.exceptions.ConnectionError,
-                redis.exceptions.BusyLoadingError
-        ):
-            logger.info("Waiting for Redis...")
-            time.sleep(1)
+    r = redis.Redis(host=redis_host, port=redis_port)
+    redis_ping(r)
+    logger.info("Redis is up and running!")
+
