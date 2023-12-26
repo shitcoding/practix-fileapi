c44fb0fb224081301bde597d1f35d7261e9e5cb4
Vera Kaynara <vera.blinova.1996@gmail.com>
2023-12-27 03:17:48 +0700

    Add dependencies

--- fileapi/src/services/file.py
+++ fileapi/src/services/file.py
@@ -6,23 +6,20 @@ import shortuuid
 from fastapi import Depends, UploadFile
 from fastapi.responses import StreamingResponse
 
-from core.config import settings
-from db.postgres import get_session
 from models.file_properties import FilePropertiesCreate
+
+from dependencies import get_file_props_service, get_s3_service
 from services.base import BaseService
-from services.file_properties import (FilePropertiesService,
-                                      get_file_properties_service)
-from services.minio import MinioService, get_minio_service
 
 
 class FileService(BaseService):
     def __init__(
         self,
-        file_properties_service: FilePropertiesService,
-        minio_service: MinioService,
+        file_properties_service: BaseService,
+        s3_service: BaseService,
     ):
         self.file_properties_service = file_properties_service
-        self.minio_service = minio_service
+        self.s3_service = s3_service
 
     async def _get_file_properties(self, file: UploadFile) -> FilePropertiesCreate:
         file_size = os.fstat(file.file.fileno()).st_size
@@ -45,12 +42,12 @@ class FileService(BaseService):
         file_properties = await self.file_properties_service.get(short_name)
         if not file_properties:
             return {'error': 'File not found'}
-        return await self.minio_service.get(file_properties.path_in_storage)
+        return await self.s3_service.get(file_properties.path_in_storage)
 
     async def save(self, file: UploadFile):
         file_properties = await self._get_file_properties(file)
         # Save the file to MinIO
-        await self.minio_service.save(file, file_properties.path_in_storage)
+        await self.s3_service.save(file, file_properties.path_in_storage)
         # Save file properties to the database
         db_file_properties = await self.file_properties_service.save(
             file_properties
@@ -63,9 +60,7 @@ class FileService(BaseService):
 
 @lru_cache
 def get_file_service(
-    file_properties_service: FilePropertiesService = Depends(
-        get_file_properties_service
-    ),
-    minio_service: MinioService = Depends(get_minio_service),
+    file_properties_service: BaseService = Depends(get_file_props_service),
+    s3_service: BaseService = Depends(get_s3_service),
 ) -> FileService:
-    return FileService(file_properties_service, minio_service)
+    return FileService(file_properties_service, s3_service)
