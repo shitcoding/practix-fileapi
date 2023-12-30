8cf5eace432d7c0184a7a2421761c2c9752403bb
Vasiliy Ivanov <shitcoder@pm.me>
2023-12-30 23:10:56 +0300

    Move migrations to src, add db table models and custom schema to alembic config, recreate initial migration

--- fileapi/src/migrations/env.py
+++ fileapi/src/migrations/env.py
@@ -1,11 +1,12 @@
 import asyncio
 from logging.config import fileConfig
 
+from alembic import context
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
-from alembic import context
+from models.file_properties import FileProperties, SQLModel
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -20,7 +21,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = FileProperties.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -40,15 +41,22 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url = config.get_main_option('sqlalchemy.url')
     context.configure(
         url=url,
         target_metadata=target_metadata,
         literal_binds=True,
-        dialect_opts={"paramstyle": "named"},
+        dialect_opts={'paramstyle': 'named'},
+        include_schemas=True,  # Added to create tables in custom schema
     )
 
     with context.begin_transaction():
+        context.execute(
+            f'create schema if not exists {target_metadata.schema};'
+        )   # Added to create tables in custom schema
+        context.execute(
+            f'set search_path to {target_metadata.schema}'
+        )   # Added to create tables in custom schema
         context.run_migrations()
 
 
@@ -67,7 +75,7 @@ async def run_async_migrations() -> None:
 
     connectable = async_engine_from_config(
         config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
+        prefix='sqlalchemy.',
         poolclass=pool.NullPool,
     )
 
