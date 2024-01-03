54a817221aa130e10ccd99c2fae5e5e5a671f714
Vera Kaynara <vera.blinova.1996@gmail.com>
2024-01-03 08:20:24 +0700

    refactoring migrations

--- fileapi/src/migrations/env.py
+++ fileapi/src/migrations/env.py
@@ -2,11 +2,10 @@ import asyncio
 from logging.config import fileConfig
 
 from alembic import context
+from models.file_properties import FileProperties
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
-
-from models.file_properties import FileProperties, SQLModel
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
