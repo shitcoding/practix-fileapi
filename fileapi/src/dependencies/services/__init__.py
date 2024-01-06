46d6b784acca7ce58a9bc9cab3f7269897628455
Vasiliy Ivanov <shitcoder@pm.me>
2024-01-06 23:51:45 +0300

    Add factories for creating services

---
+++ fileapi/src/dependencies/services/__init__.py
@@ -0,0 +1,3 @@
+from .file_properties_service_factory import create_file_properties_service
+from .minio_service_factory import create_minio_service
+from .file_service_factory import create_file_service
