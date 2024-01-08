5c12b8b9796515f71081334c4908de7b65a56595
Vasiliy Ivanov <shitcoder@pm.me>
2024-01-08 23:46:56 +0300

    Add exception handling for uploading files from django admin

--- django/movies/storage.py
+++ django/movies/storage.py
@@ -1,18 +1,27 @@
+import logging
+
 import requests
 
 from django.conf import settings
 from django.core.files.storage import Storage
 from django.core.files.uploadedfile import InMemoryUploadedFile
 
+logger = logging.getLogger(__name__)
+
 
 class CustomStorage(Storage):
     def _save(self, name, content: InMemoryUploadedFile):
         url = f'{settings.FILEAPI_HOST}{settings.FILEAPI_UPLOAD_URL}'
-        r = requests.post(
-            url=url,
-            files={'file': (content.name, content, content.content_type)},
-        )
-        return r.json().get('file_properties').get('short_name')
+        try:
+            r = requests.post(
+                url=url,
+                files={'file': (content.name, content, content.content_type)},
+            )
+            r.raise_for_status()
+            return r.json().get('file_properties').get('short_name')
+        except requests.exceptions.RequestException as e:
+            logger.error(f'Error while saving file: {e}')
+            return None
 
     def url(self, short_name):
         return f'{settings.FILEAPI_DOWNLOAD_URL}{short_name}'
